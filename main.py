37f71501ccec438ca222b715aa1fe4a03712025b
Chench824 <57470356+Chench824@users.noreply.github.com>
2022-10-19 14:31:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,8 +58,7 @@ def getWeather():
         return
     else:
         global K, type
-#         url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + area
-        url = 'http://wthrcdn.etouch.cn/weather_mini?city=广州'
+        url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + area
         hea = {'User-Agent': 'Mozilla/5.0'}
         r = requests.get(url=url, headers=hea)
         if r.status_code == 200:
