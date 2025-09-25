69321349e4a0a298082cb5e4d449af96fc7c2d69
Chench824 <57470356+Chench824@users.noreply.github.com>
2025-09-25 15:42:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,6 @@ def login(user, password):
         "token": "access"
     }
     r1 = requests.post(url1, data=data1, headers=headers, allow_redirects=False)
-    print(t1)
     location = r1.headers["Location"]
     print(location)
     try:
